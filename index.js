39ded5924f0108b11104493fc8732c57ba67dba8
Henry Miller <mail.henrym+git@gmail.com>
2022-01-10 12:55:12 -0500

    commit tree 5bf2ea842aa0c797ed17d6398465e4d8dc6541bb

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2;
 
-// Write your code here
+const num3 = 16;
+const num4 = 12;
+const mod = num3 % num4;
+
+const random = 1;
+
+const max = Math.max(1, 2, 3, 4, 20);
